73a21f8b01a03bd57063c58e0f516d641590ec98
Invader Zim <InvaderZim62>
2026-03-20 21:55:40 -0700

    Improved camera control significantly by using built-in method to point camera at an object/location.

--- TestCameraControl/ViewController.swift
+++ TestCameraControl/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 import RealityKit
 
+struct Constant {
+    static let cameraDistance: Float = 5  // can't be at 0, for pinch to work
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var arView: ARView!
@@ -24,61 +28,40 @@ class ViewController: UIViewController {
         arView.environment.background = .color(.lightGray)
         arView.scene.addAnchor(worldAnchor)
 
-        camera.position.z = 5  // can't be at 0, for pinch to work
-        icPosition = camera.position
+        camera.position.z = Constant.cameraDistance
         worldAnchor.addChild(camera)
 
         let material1 = SimpleMaterial(color: .green, isMetallic: false)
-        let cubeEntity = ModelEntity(mesh: .generateBox(size: [0.2, 0.2, 0.2]))
-        cubeEntity.model?.materials = [material1]
-        cubeEntity.position = [0, 0, 0]
-        worldAnchor.addChild(cubeEntity)
+        let greenCube = ModelEntity(mesh: .generateBox(size: [0.2, 0.2, 0.2]))
+        greenCube.model?.materials = [material1]
+        greenCube.position = [0, 0, 0]
+        worldAnchor.addChild(greenCube)
 
         let material2 = SimpleMaterial(color: .red, isMetallic: false)
-        let floorEntity = ModelEntity(mesh: .generateBox(size: [2, 0.1, 2]))
-        floorEntity.model?.materials = [material2]
-        floorEntity.position = [0, -1, 0]
-        worldAnchor.addChild(floorEntity)
+        let redFloor = ModelEntity(mesh: .generateBox(size: [2, 0.1, 2]))
+        redFloor.model?.materials = [material2]
+        redFloor.position = [0, -1, 0]
+        worldAnchor.addChild(redFloor)
 
         let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
         arView.addGestureRecognizer(pan)
     }
-    
-    var icPosition = SIMD3<Float>(0, 0, 0)
-    
+        
     @objc func handlePan(recognizer: UIPanGestureRecognizer) {
-        switch recognizer.state {
-        case .changed:
-            let translate = recognizer.translation(in: recognizer.view)
-            let deltaUp = Float(translate.y / 200)
-            let deltaRight = Float(translate.x / 100)
-            pitch += deltaUp
-            yaw += deltaRight
-            recognizer.setTranslation(.zero, in: recognizer.view)
-            
-            print(String(format: "dUp: %.3f, pitch: %.1f, dRight: %.3f, yaw: %.1f", deltaUp * 57.3, pitch * 57.3, deltaRight * 57.3, yaw * 57.3))
-            
-            // move the camera around origin
-            camera.position = [-icPosition.z * sin(yaw),
-                                icPosition.z * sin(pitch),
-                                icPosition.z * cos(pitch) * cos(yaw)]
-            
-            // rotate the camera to point at origin
-            camera.setOrientation(Transform(pitch: -pitch, yaw: -yaw, roll: .zero).rotation, relativeTo: worldAnchor.anchor)
-            
-            // want: rotate scene around camera
-//            worldAnchor.setOrientation(Transform(pitch: angleX, yaw: angleY, roll: .zero).rotation, relativeTo: camera.anchor)  // same, but light source spinning around objects
-//            camera.setOrientation(Transform(pitch: angleX, yaw: angleY, roll: .zero).rotation, relativeTo: camera.anchor)  // same as original
-//            camera.setPosition(Transform(pitch: pitch, yaw: yaw, roll: .zero).translation, relativeTo: worldAnchor.anchor)  // shoots off screen
-        default: break
-        }
+        let translate = recognizer.translation(in: recognizer.view)
+        let deltaUp = Float(translate.y / 200)
+        let deltaRight = Float(translate.x / 100)
+        recognizer.setTranslation(.zero, in: recognizer.view)
+        
+        pitch += deltaUp
+        yaw += deltaRight
+        
+        // move the camera around the center
+        camera.position = [-Constant.cameraDistance * sin(yaw),
+                            Constant.cameraDistance * sin(pitch),
+                            Constant.cameraDistance * cos(pitch) * cos(yaw)]
+        
+        // point the camera at the center
+        camera.look(at: .zero, from: camera.position, relativeTo: nil)
     }
-    
-//    // rotate camera around scene x-axis, while continuing to point at scene center
-//    private func rotateCameraAroundBoardCenter(deltaAngle: CGFloat) {
-//        cameraNode.transform = SCNMatrix4Rotate(cameraNode.transform, Float(deltaAngle), 1, 0, 0)
-//        let cameraAngle = CGFloat(cameraNode.eulerAngles.x)
-//        let cameraDistance = CGFloat(3 * Box.width)
-//        cameraNode.position = SCNVector3(0, -cameraDistance * sin(cameraAngle), cameraDistance * cos(cameraAngle))
-//    }
 }
