f628addbd74b7ac37cba252456364b98101cba74
Invader Zim <InvaderZim62>
2026-03-23 13:32:44 -0700

    Use quaternion rates, rather than camera.look, since the later always keeps the camera right-side up.

--- TestCameraControl/ViewController.swift
+++ TestCameraControl/ViewController.swift
@@ -6,9 +6,16 @@
 //
 //  Test of my version of pan and pinch.
 //
+//  Useful properties:
+//    let eulerAngles = camera.transform.matrix.eulerAngles  // [x, y, z], pitch, -yaw, -roll
+//    let quaternion = simd_quatf(camera.transform.matrix).vector  // [x, y, z, w], where w is the rotation
+//    -or-
+//    let quaternion = camera.orientation.vector
+//
 
 import UIKit
 import RealityKit
+import ARKit
 
 struct Constant {
     static let cameraDistance: Float = 5  // can't be at 0, for pinch to work
@@ -20,29 +27,29 @@ class ViewController: UIViewController {
     
     let worldAnchor = AnchorEntity()
     let camera = PerspectiveCamera()
-    var pitch: Float = 0.0
-    var yaw: Float = 0.0
     var cameraDistance: Float = Constant.cameraDistance
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         arView.cameraMode = .nonAR
+        arView.debugOptions = [.showWorldOrigin, .showPhysics]  // use .showPhysics with .generateCollisionShapes
         arView.environment.background = .color(.lightGray)
         arView.scene.addAnchor(worldAnchor)
 
         camera.position.z = Constant.cameraDistance
         worldAnchor.addChild(camera)
 
-        let material1 = SimpleMaterial(color: .green, isMetallic: false)
+        let cubeMaterial = SimpleMaterial(color: .green, isMetallic: false)
         let greenCube = ModelEntity(mesh: .generateBox(size: [0.2, 0.2, 0.2]))
-        greenCube.model?.materials = [material1]
+        greenCube.model?.materials = [cubeMaterial]
         greenCube.position = [0, 0, 0]
+        greenCube.generateCollisionShapes(recursive: false)  // use with debugOptions
         worldAnchor.addChild(greenCube)
 
-        let material2 = SimpleMaterial(color: .red, isMetallic: false)
+        let floorMaterial = SimpleMaterial(color: .red, isMetallic: false)
         let redFloor = ModelEntity(mesh: .generateBox(size: [2, 0.1, 2]))
-        redFloor.model?.materials = [material2]
+        redFloor.model?.materials = [floorMaterial]
         redFloor.position = [0, -1, 0]
         worldAnchor.addChild(redFloor)
 
@@ -62,17 +69,49 @@ class ViewController: UIViewController {
             let deltaUp = Float(translate.y / 200)
             let deltaRight = Float(translate.x / 100)
             recognizer.setTranslation(.zero, in: recognizer.view)
+
+            let camereEulers = camera.transform.matrix.eulerAngles  // [x, y, z], pitch, -yaw, -roll
+            print(String(format: "camera.eulerAngles: (x: %0.2f, y: %0.2f, z: %0.2f)", camereEulers.x * 57.3, camereEulers.y * 57.3, camereEulers.z * 57.3))
             
-            pitch += deltaUp
-            yaw += deltaRight
+//            // move the camera around the center
+//            camera.position = [-cameraDistance * sin(yaw),
+//                                cameraDistance * sin(pitch),
+//                                cameraDistance * cos(pitch) * cos(yaw)]
+
+            let p = deltaRight * sin(camereEulers.x)  // roll about camera z
+            let q = -deltaUp  // pitch about camera x
+            let r = -deltaRight * cos(camereEulers.x)  // yaw about camera y
+            
+            let quatIC = camera.orientation.vector  // [x, y, z, w], where w is the rotation
+            
+            // quaternion rates
+            let qwDot = (-quatIC.x * q - quatIC.y * r - quatIC.z * p) / 2
+            let qxDot = ( quatIC.w * q - quatIC.z * r + quatIC.y * p) / 2
+            let qyDot = ( quatIC.z * q + quatIC.w * r - quatIC.x * p) / 2
+            let qzDot = (-quatIC.y * q + quatIC.x * r + quatIC.w * p) / 2
+            
+            // intergate
+            var qw = quatIC.w + qwDot
+            var qx = quatIC.x + qxDot
+            var qy = quatIC.y + qyDot
+            var qz = quatIC.z + qzDot
+            
+            // normalize to prevent error growth (probably not needed)
+            let qnorm = sqrt(pow(qw, 2) + pow(qx, 2) + pow(qy, 2) + pow(qz, 2))
+            
+            qw /= qnorm
+            qx /= qnorm
+            qy /= qnorm
+            qz /= qnorm
+            
+            camera.orientation = simd_quatf(ix: qx, iy: qy, iz: qz, r: qw)
+            
+            let eulerAngles = camera.transform.matrix.eulerAngles  // [x, y, z], pitch, -yaw, -roll
             
             // move the camera around the center
-            camera.position = [-cameraDistance * sin(yaw),
-                                cameraDistance * sin(pitch),
-                                cameraDistance * cos(pitch) * cos(yaw)]
-            
-            // point the camera at the center
-            camera.look(at: .zero, from: camera.position, relativeTo: nil)
+            camera.position = [ cameraDistance * cos(eulerAngles.x) * sin(eulerAngles.y),
+                               -cameraDistance * sin(eulerAngles.x),
+                                cameraDistance * cos(eulerAngles.x) * cos(eulerAngles.y)]
         default: return
         }
     }
@@ -86,5 +125,31 @@ class ViewController: UIViewController {
             recognizer.scale = 1
         default: break
         }
+    }
+    
+    // direction cosine matrix
+    public func eul2rot(theta: simd_float3) -> simd_float3x3 {
+        var R = simd_float3x3(columns: (
+            [cos(theta[1])*cos(theta[2]),
+             sin(theta[0])*sin(theta[1])*cos(theta[2]) - sin(theta[2])*cos(theta[0]),
+             sin(theta[1])*cos(theta[0])*cos(theta[2]) + sin(theta[0])*sin(theta[2])],
+            [sin(theta[2])*cos(theta[1]),
+             sin(theta[0])*sin(theta[1])*sin(theta[2]) + cos(theta[0])*cos(theta[2]),
+             sin(theta[1])*sin(theta[2])*cos(theta[0]) - sin(theta[0])*cos(theta[2])],
+            [-sin(theta[1]),
+              sin(theta[0])*cos(theta[1]),
+              cos(theta[0])*cos(theta[1])]))
+        R = R.transpose
+        return R
+    }
+}
+
+extension simd_float4x4 {
+    var eulerAngles: simd_float3 {
+        simd_float3(
+            x: asin(-self[2][1]),
+            y: atan2(self[2][0], self[2][2]),
+            z: atan2(self[0][1], self[1][1])
+        )
     }
 }
